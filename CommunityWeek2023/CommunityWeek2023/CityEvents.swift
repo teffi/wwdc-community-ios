bf7ca4db616ece587841182cca8959689f0b4b5a
Stef <stef.devwork@gmail.com>
2023-05-30 18:49:29 +0800

    Update to hide navigation link arrow icon

--- CommunityWeek2023/CommunityWeek2023/CityEvents.swift
+++ CommunityWeek2023/CommunityWeek2023/CityEvents.swift
@@ -10,28 +10,34 @@ import SafariServices
 
 struct CityEvents: View {
   let cityData: EventsPerCity
-  @State var selectedEvent: Event?
   
   var body: some View {
     List {
       Section {
         ForEach(cityData.events) { event in
-          NavigationLink(value: event) {
-            VStack(alignment: .leading, spacing: 6) {
-              HStack {
-                Text(event.description)
-                  .font(.headline)
-                  .fontWeight(.bold)
-                Spacer()
-                Image(systemName: "rectangle.fill.on.rectangle.fill")
-              }
-              Text(event.date)
-                .font(.caption)
+          VStack(alignment: .leading, spacing: 6) {
+            HStack {
+              Text(event.description)
+                .font(.headline)
                 .fontWeight(.bold)
+              Spacer()
+              Image(systemName: "rectangle.fill.on.rectangle.fill")
             }
-            .padding([.top,.bottom], 10)
-            .padding([.leading,.trailing], 8)
+            Text(event.date)
+              .font(.caption)
+              .fontWeight(.bold)
           }
+          .padding([.top,.bottom], 10)
+          .padding([.leading,.trailing], 8)
+          .background {
+            NavigationLink(value: event) {
+              EmptyView()
+            }
+            // Why set to 0: NavigationLink adds an arrow icon to a list item.
+            // To hide the icon, we set 0 opacity and place navlink as background so it wont take up any space on the row.
+            .opacity(0)
+          }
+
         }
 
       } header: {
@@ -46,6 +52,8 @@ struct CityEvents: View {
     })
   }
 }
+
+
 
 //struct CityEvents_Previews: PreviewProvider {
 //  static var previews: some View {
